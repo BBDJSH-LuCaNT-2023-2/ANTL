e5f7be190bb1e99f4131bed3715739d6e00da1f5
david <david.marquis@ucalgary.ca>
2020-09-30 13:41:52 -0600

    Single function for computing unit group relations

--- include/ANTL/IndexCalculus/IndCalc/IndCalc.hpp
+++ include/ANTL/IndexCalculus/IndCalc/IndCalc.hpp
@@ -9,58 +9,56 @@
 #include "ANTL/Interface/Multiplicative.hpp"
 #include "ANTL/IndexCalculus/FactorBase/FactorBase.hpp"
 #include "ANTL/IndexCalculus/RelationGenerator/RelationGenerator.hpp"
+#include "ANTL/Interface/OrderInvariants.hpp"
 #include <ANTL/common.hpp>
 
 using namespace ANTL;
 
-template <class R> // type of regulator
-class IndCalc {
+template <class T, class R> // type of unit, type of regulator
+class IndCalc: IClassGroup, IClassNumber, IUnitGroup<T>, IRegulator<R> {
 public:
-  IndCalc<R>(IOrder const &order, std::map<std::string, std::string> const &params) {setup_cg_mat(order, params);}
+  IndCalc<T,R>(IOrder const &order, std::map<std::string, std::string> const &params) {setup_mat(order, params);}
 
-  // subclasses should implement class_number to return the class_number
-  virtual NTL::ZZ class_number();
-  // subclasses should implement class_group to return the class_group
-  virtual std::vector<NTL::ZZ> class_group();
+  // subclasses should implement class_number to return the class_number, class group, unit group, regulator
 
-  FactorBase cg_factor_base;
-  RelationGenerator cg_relations_generator;
-  NTL::Mat<long> cg_mat;
+  FactorBase factor_base;
+  RelationGenerator relations_generator;
+  NTL::Mat<ZZ> rels_mat;
 
-  void setup_cg_fac_base(IOrder const &order, std::map<std::string, std::string> const &params);
-  void setup_cg_relations(IOrder const &order, std::map<std::string, std::string> const &params);
-  void setup_cg_mat(IOrder const &order, std::map<std::string, std::string> const &params);
+  void setup_fac_base(IOrder const &order, std::map<std::string, std::string> const &params);
+  void setup_relations(IOrder const &order, std::map<std::string, std::string> const &params);
+  void setup_mat(IOrder const &order, std::map<std::string, std::string> const &params);
 
 protected:
-  // subclasses should implement compute_cg_fac_base to set the cg_factor_base variable
-  virtual void compute_cg_fac_base(IOrder const &order, std::map<std::string, std::string> const &params) {};
-  // subclasses should implement compute_cg_relations to set the cg_relations variable
-  virtual void compute_cg_relations(IOrder const &order, std::map<std::string, std::string> const &params) {};
-  // subclasses should implement compute_cg_mat to set the cg_mat variable
-  virtual void compute_cg_mat(IOrder const &order, std::map<std::string, std::string> const &params) {};
+  // subclasses should implement compute_fac_base to set the factor_base variable
+  virtual void compute_fac_base(IOrder const &order, std::map<std::string, std::string> const &params) {};
+  // subclasses should implement compute_relations to set the cg_relations variable
+  virtual void compute_relations(IOrder const &order, std::map<std::string, std::string> const &params) {};
+  // subclasses should implement compute_mat to set the cg_mat variable
+  virtual void compute_mat(IOrder const &order, std::map<std::string, std::string> const &params) {};
 };
 
-template <class R> void IndCalc<R>::setup_cg_fac_base(const IOrder &order, const std::map<std::string, std::string> &params) {
-  compute_cg_fac_base(order, params);
+template <class T, class R> void IndCalc<T,R>::setup_fac_base(const IOrder &order, const std::map<std::string, std::string> &params) {
+  compute_fac_base(order, params);
   //TODO
-//  if (cg_factor_base.size() == 0) {
+//  if (factor_base.size() == 0) {
     // raise error
 //  }
 }
 
-template <class R> void IndCalc<R>::setup_cg_relations(IOrder const &order, std::map<std::string, std::string> const &params) {
-  setup_cg_fac_base(order, params);
-  compute_cg_relations(order, params);
+template <class T, class R> void IndCalc<T,R>::setup_relations(IOrder const &order, std::map<std::string, std::string> const &params) {
+  setup_fac_base(order, params);
+  compute_relations(order, params);
   //TODO
 //  if (cg_relations.size() == 0) {
     // raise error
 //  }
 }
 
-template <class R> void IndCalc<R>::setup_cg_mat(IOrder const &order, std::map<std::string, std::string> const &params) {
-  setup_cg_relations(order, params);
-  compute_cg_mat(order, params);
-  if (cg_mat.NumRows() == 0 or cg_mat.NumCols() == 0) {
+template <class T, class R> void IndCalc<T,R>::setup_mat(IOrder const &order, std::map<std::string, std::string> const &params) {
+  setup_relations(order, params);
+  compute_mat(order, params);
+  if (rels_mat.NumRows() == 0 or rels_mat.NumCols() == 0) {
     // raise error
   }
 }
--- src/IndexCalculus/IndCalc/QuadIndCalc_impl.hpp
+++ src/IndexCalculus/IndCalc/QuadIndCalc_impl.hpp
@@ -11,12 +11,12 @@
 #include "ANTL/IndexCalculus/FactorBase/QuadFactorBase.hpp"
 
 template <class T, class R>
-void QuadIndCalc<T,R>::compute_cg_fac_base(IOrder const &quad_order, std::map<std::string, std::string> const &params) {
+void QuadIndCalc<T,R>::compute_fac_base(IOrder const &quad_order, std::map<std::string, std::string> const &params) {
   //cf ANTL/include/ANTL/quadratic/index_calculus/factor_base.hpp
   std::string fb_size_str = "size_fb";
   if ( params.find(fb_size_str) == params.end() ) {
     NTL::ZZ fb_size(NTL::INIT_VAL, params.find(fb_size_str)->first);
-    IndCalc<R>::cg_factor_base = QuadFactorBase<T>(fb_size);
+    IndCalc<T,R>::factor_base = QuadFactorBase<T>(fb_size);
 
     // get factor basis elements using QuadFactorBase
   } else {
@@ -25,12 +25,12 @@ void QuadIndCalc<T,R>::compute_cg_fac_base(IOrder const &quad_order, std::map<st
 };
 
 template <class T, class R>
-void QuadIndCalc<T,R>::compute_cg_relations(IOrder const &quad_order, std::map<std::string, std::string> const &params) {
+void QuadIndCalc<T,R>::compute_relations(IOrder const &quad_order, std::map<std::string, std::string> const &params) {
   //cf ANTL/src/quadratic/qo_relation_generator_impl.hpp
   std::string num_relations_str = "num_relations";
   if ( params.find(num_relations_str) == params.end() ) {
     NTL::ZZ num_relations(NTL::INIT_VAL, params.find(num_relations_str)->first);
-    IndCalc<R>::cg_relation_generator = QuadRelationGenerator<T>(quad_order, params, IndCalc<R>::cg_factor_base);
+    IndCalc<T,R>::relation_generator = QuadRelationGenerator<T>(quad_order, params, IndCalc<T,R>::factor_base);
 
     // find relations by calling RelationGenerator's get_relation function
     // Call this function num_relations times
@@ -41,9 +41,9 @@ void QuadIndCalc<T,R>::compute_cg_relations(IOrder const &quad_order, std::map<s
 };
 
 template <class T, class R>
-void QuadIndCalc<T,R>::compute_cg_mat(IOrder const &quad_order, std::map<std::string, std::string> const &params) {
+void QuadIndCalc<T,R>::compute_mat(IOrder const &quad_order, std::map<std::string, std::string> const &params) {
   //cf ANTL/include/ANTL/quadratic/index_calculus/qo_relation_matrix.hpp
-  IndCalc<R>::cg_mat.SetDims(IndCalc<R>::cg_relations.size(), IndCalc<R>::cg_factor_base.size());
+  IndCalc<T,R>::rels_mat.SetDims(IndCalc<T,R>::cg_relations.size(), IndCalc<T,R>::factor_base.size());
 
   // set matrix using functions in QuadRelation
 };
