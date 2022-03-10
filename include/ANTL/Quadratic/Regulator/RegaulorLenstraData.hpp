0972d2422b1f63d1ac37f829a49f02014d1a75aa
austin.docherty <austin.docherty@ucalgary.ca>
2022-03-10 13:36:44 -0500

    Initial commit for porting regulator computation (Lenstra's Method). Data and Compute class files created.

---
+++ include/ANTL/Quadratic/Regulator/RegaulorLenstraData.hpp
@@ -0,0 +1,12 @@
+#ifndef REGULATOR_LENSTRA_DATA_H
+#define REGULATOR_LENSTRA_DATA_H
+
+#include <ANTL/Quadratic/QuadraticOrder.hpp>
+#include <ANTL/common.hpp>
+
+NTL_CLIENT
+using namespace ANTL;
+
+namespace ANTL {
+template <class T> class RegulatorLenstraData {};
+} // namespace ANTL
