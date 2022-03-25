4be9ce1d78aae0c7b28a0091f222d68b185bc2f9
Zack Baker <zack.baker@protonmail.ch>
2022-03-25 12:53:56 -0600

    Restructured XGCD test file
    Restructured the xgcd unit test file to begin adding tests  for different XGCD methods

--- tests/XGCD/xgcd_test.cpp
+++ tests/XGCD/xgcd_test.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "tests/catch.hpp"
+
 // qvm matrix headers
 #include <boost/qvm/mat.hpp>
 #include <boost/qvm/mat_traits.hpp>
@@ -18,13 +20,81 @@ using std::cout;
 using std::endl;
 NTL_CLIENT
 
-int main(){
+/*int main(){
 long a,b,r,s,g;
 a = 3;
 b= 5;
 XGCD(g, r,s,a,b);
 cout << g << " = " << a << "*" << r <<  " + " << b << "*" << s << endl;
 
-
+XGCD_PLAIN(r,s,a,b);
+cout << r << " " << s << " " << a << " " << b << "\n";
 return 0;
+}*/
+
+TEST_CASE("[XGCD]: int64_t PLAIN"){
+    int64_t a,b,r,s;
+
+    //basic initial test: (3,5) = 1
+    a = 3;
+    b = 5;
+    XGCD_PLAIN(r,s,a,b);
+    REQUIRE(a == 1);
+
+    //reverse magnitude test: (5,3) = 1
+    a = 5;
+    b = 3;
+    XGCD_PLAIN(r,s,a,b);
+    REQUIRE(a == 1);
+
+
+    //common factor test: (5,20) = 5
+    a = 5;
+    b = 20;
+    XGCD_PLAIN(r,s,a,b);
+    REQUIRE(a == 5);
+
+    //unit test: (1, 999) = 1
+    a = 1;
+    b = 999;
+    XGCD_PLAIN(r,s,a,b);
+    REQUIRE(a == 1);
+
+    //sign permutation tests: (+/- 3, +/- 6)
+    a = 3;
+    b = 6;
+    XGCD_PLAIN(r,s,a,b);
+    REQUIRE(a == 3);
+
+    a = -3;
+    b = 6;
+    XGCD_PLAIN(r,s,a,b);
+    REQUIRE(a == 3);
+
+    a = 3;
+    b = -6;
+    XGCD_PLAIN(r,s,a,b);
+    REQUIRE(a == 3);
+
+    a = -3;
+    b = -6;
+    XGCD_PLAIN(r,s,a,b);
+    REQUIRE(a == 3);
+
+    //arbitrary large (2^32 < a,b < 2^63) positive test: (3166167471260038366, 2078992898117306689) = 1
+    a = 3166167471260038366;
+    b = 2078992898117306689;
+    XGCD_PLAIN(r,s,a,b);
+    REQUIRE(a == 1);
+
+    //arbitrary large (-2^32 > a,b > 2^63) negative test: (-3867470587490682194, -6531477986582055176) = 2 
+    a = -3867470587490682194;
+    b = -6531477986582055176;
+    XGCD_PLAIN(r,s,a,b);
+    REQUIRE(a == 2);
+
+
+
+
+
 }
