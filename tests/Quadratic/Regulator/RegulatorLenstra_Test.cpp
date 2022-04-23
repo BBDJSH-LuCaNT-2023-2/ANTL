4dc93ce51d14b5314a0d638ef60b7cde853901f1
austin.docherty <austin.docherty@ucalgary.ca>
2022-04-23 17:06:36 -0400

    Begins the process of porting over class_group_bsgs from ANTL-Import. Creates a basic folder and header file.

--- tests/Quadratic/Regulator/RegulatorLenstra_Test.cpp
+++ tests/Quadratic/Regulator/RegulatorLenstra_Test.cpp
@@ -8,12 +8,14 @@
 using namespace NTL;
 using namespace ANTL;
 
+bool DBG_LENSTRA_TEST = true;
+
 int discriminants[10] = {55661, 63361, 72673, 86341, 38593,
                          54269, 41513, 74021, 45677, 17909};
 
 double correct_regulators[10] = {
     25.4538649123, 17.5077374033, 357.702597578, 148.556426268, 105.442818369,
-    49.458153743, 130.11243841,  62.8055127479, 54.4615644632, 44.1429273524};
+    49.458153743,  130.11243841,  62.8055127479, 54.4615644632, 44.1429273524};
 
 double computed_regulators[10];
 bool computed_correctly[10];
@@ -55,19 +57,21 @@ TEST_CASE("RegulatorLenstra<ZZ>: Does it work?", "[RegulatorLenstra]") {
 
   bool test_bool = true;
 
-  std::cout << "CASE"
-            << std::setw(8 ) << "RESULT"
-            << std::setw(9 ) << "CORRECT"
-            << std::setw(10) << "COMPUTED"
-            << std::setw(7 ) << "DELTA" << std::endl;
-  std::cout << std::setw(38) << std::setfill('=') << "" << std::endl;
+  if (DBG_LENSTRA_TEST) {
+    std::cout << "CASE" << std::setw(8) << "RESULT" << std::setw(9) << "CORRECT"
+              << std::setw(10) << "COMPUTED" << std::setw(7) << "DELTA"
+              << std::endl;
+    std::cout << std::setw(38) << std::setfill('=') << "" << std::endl;
+  }
 
   for (int i = 0; i < 10; i++) {
-    std::cout << std::setfill('0') << std::setw(4) << i + 1 << std::setfill(' ')
-              << std::setw(6 ) << computed_correctly[i]
-              << std::setw(11) << correct_regulators[i]
-              << std::setw(10) << computed_regulators[i]
-              << std::setw(7 ) << discriminants[i] << std::endl;
+    if (DBG_LENSTRA_TEST) {
+      std::cout << std::setfill('0') << std::setw(4) << i + 1
+                << std::setfill(' ') << std::setw(6) << computed_correctly[i]
+                << std::setw(11) << correct_regulators[i] << std::setw(10)
+                << computed_regulators[i] << std::setw(7) << discriminants[i]
+                << std::endl;
+    }
 
     test_bool = test_bool && computed_correctly[i];
   }
